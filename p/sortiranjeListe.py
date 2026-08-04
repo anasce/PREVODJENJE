dd3444e6a9b67adef6a4f675e624e9a694d5cc8c
anasce <97723156+anasce@users.noreply.github.com>
2026-08-04 08:48:07 +0200

    Delete p/sortiranjeListe.py

--- p/sortiranjeListe.py
+++
@@ -1,9 +0,0 @@
-
-sortirana_lista = sorted(EXACT, key=lambda x: (-len(str(x[0])), str(x[0]).lower()))
-    
-# Upis u fajl sa UTF-8 enkodingom zbog naših slova (č, ć, š, đ, ž)
-with open('sortirani_parovi.txt', 'w', encoding='utf-8') as f:
-        f.write("EXACT = [\n")
-        for tpl in sortirana_lista:
-            f.write(f"    {tpl},\n")
-        f.write("]\n")
